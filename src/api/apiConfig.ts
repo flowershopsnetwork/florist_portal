abcea1bea2e3235ba651473c777405f4758653b3
Dominic Anuta Vega <davega12.dv@gmail.com>
2025-04-28 21:02:31 +0800

    environment

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-  BASE_URL: "http://127.0.0.1:8000/api/v1",
+  BASE_URL: "https://florists-api.flowershops.network/api/v1",
   IMAGE_BASE_URL: "https://dev-florists.flowershops.network/uploads",
 };
